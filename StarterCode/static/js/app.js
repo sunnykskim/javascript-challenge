c657e21a1de54ad688c8144c92985e026de6eb1c
sunnykskim <51854898+sunnykskim@users.noreply.github.com>
2019-10-27 19:37:17 -0400

    push

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,44 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+/////// Populate Table  ///////
+
+tbody = d3.select("tbody")
+
+// get data for the table + loop 
+function displayData(data){ 
+    tbody.text("")
+    data.forEach(function(sighting){
+    new_tr = tbody.append("tr")
+    Object.entries(sighting).forEach(function([key, value]){
+        new_td = new_tr.append("td").text(value)	
+    })
+})}
+
+displayData(tableData)
+
+
+/////// Filter ///////
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+
+  var filteredData = tableData.filter(sighting => sighting.datetime===inputValue);
+
+  console.log(filteredData)
+  displayData(filteredData)
+});
+
+
+
